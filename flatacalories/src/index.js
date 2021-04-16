650c5ce9b31499404f3371d43b643495e549e694
Alex Kravecas <akravecas@yahoo.com>
2021-04-16 11:36:01 -0400

    completed

---
+++ flatacalories/src/index.js
@@ -0,0 +1,117 @@
+const url = 'http://localhost:3000/characters/'
+
+getFetch()
+
+
+function getFetch(){
+    fetch(url)
+    .then((resp)=> resp.json())
+    .then(function(data){
+        console.log(data);
+        data.forEach(createNames)
+    })
+}
+
+function createNames(data){
+    const spanner = document.createElement('span')
+    spanner.innerText=data.name
+    document.querySelector('#character-bar').appendChild(spanner)
+
+    spanner.addEventListener('click', ()=>{
+        addInfo(data)
+    })
+    
+}
+
+function addInfo(data){
+    // console.log(data.image)
+    const imgs = document.querySelector('#image')
+    imgs.src = data.image
+    
+    const name = document.querySelector('#name')
+    name.innerText = data.name
+
+    const calories = document.querySelector('#calories')
+    calories.innerText = data.calories
+
+    const ids = document.createElement('span')
+    ids.innerText = data.id
+    document.querySelector('#CharacterId').appendChild(ids)
+
+    const calBar = document.querySelector('#calories-form')
+    calBar.addEventListener('submit', (e)=> {
+        e.preventDefault()
+        // let animalId = ""
+        console.log(data)
+        
+        animalId = e.target[0].innerText
+        parseInt(animalId)
+        console.log(animalId)
+
+        const newCals = e.target[1].value
+
+        const existingCals = +document.querySelector('#calories').innerText
+
+        const totCals = parseInt(existingCals) + parseInt(newCals)
+
+        console.log(url+animalId)
+
+        // debugger
+
+        // e.target.reset()
+        calsObj = {calories: totCals}
+    
+        const reqObj = {
+            headers: {"Content-Type": "application/json"},
+            method: "PATCH",
+            body: JSON.stringify(calsObj)
+            }
+    
+            fetch(url+animalId,reqObj)
+            .then((resp)=>resp.json())
+            .then(function(data){
+                document.querySelector('#calories').innerText = data.calories
+            })
+    
+
+        // addCalories(e)
+        // e.target.reset()
+    })
+        
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addCalories(e){
+//     console.log(e)
+//     const addCals = e.target[1].value
+//     // e.target.reset()
+//     newCals = {calories: addCals}
+
+//     const reqObj = {
+//         headers: {"Content-Type": "application/json"},
+//         method: "PATCH",
+//         body: JSON.stringify(newCals)
+//         }
+
+//         fetch(url+btn.id,reqObjFirst)
+//         .then((resp)=>resp.json())
+//         .then(function(data){})
+
+    
+    
+    
+// }
