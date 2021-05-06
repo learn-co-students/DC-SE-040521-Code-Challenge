74d9f4edbba1d2ae990faac9bad1fb0a69aea463
Amy Mangor <aemangor@gmail.com>
2021-05-06 11:34:59 -0400

    Submission

--- bank-of-flatiron/src/components/AccountContainer.js
+++ bank-of-flatiron/src/components/AccountContainer.js
@@ -4,12 +4,40 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+state = {
+  date: "",
+  description: "",
+  category: "",
+  amount: 0,
+  transactions:[]
+
+}
+componentDidMount() {
+  fetch('http://localhost:6001/transactions')
+  .then(res => res.json())
+  .then(transactions => this.setState({transactions: transactions}))
+}
+
+addTransaction = transaction => this.setState({transactions: [...this.state.transactions, transaction]})
+
+searchText = (text) => {
+  this.setState({
+    searchText:text
+  })
+}
+
+findTransaction = () => {
+  const filterTransaction = this.state.transactions.filter((transaction) => transaction.description.includes(this.state.searchText))
+  return filterTransaction
+}
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchText={this.searchText}  />
+        <AddTransactionForm addTransaction={this.addTransaction} />
+        <TransactionsList transactions={this.state.transactions} transactions={this.findTransaction()} />
       </div>
     );
   }
