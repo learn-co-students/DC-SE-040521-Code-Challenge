543f052550ca1daafc871767eca49cd4cab508ce
bwennuh <brennabruening@gmail.com>
2021-05-06 09:52:59 -0400

    Completed core deliverables

--- bank-of-flatiron/src/components/AccountContainer.js
+++ bank-of-flatiron/src/components/AccountContainer.js
@@ -3,13 +3,45 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const baseURL = "http://localhost:6001/transactions/"
+
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    searchText: ""
+  }
+
+  componentDidMount(){
+    fetch(baseURL)
+    .then(resp => resp.json())
+    .then(transactions => this.setState({transactions}))
+  }
+
+  searchTransactions = (text) => {
+    this.setState({
+      searchText: text
+    })
+  }
+
+  filterTransacations = () => {
+    const filteredTransactions = this.state.transactions.filter(transaction => transaction.description.toLowerCase().includes(this.state.searchText.toLowerCase()))
+    return filteredTransactions
+  }
+
+  addNewTransaction = (newObj) => {
+    this.setState({
+      transactions: [...this.state.transactions, newObj]
+    })
+  }
+
   render() {
+
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchTransactions={this.searchTransactions} />
+        <AddTransactionForm addNewTransaction={this.addNewTransaction} />
+        <TransactionsList transactions={this.filterTransacations()} />
       </div>
     );
   }
--- bank-of-flatiron/src/components/AddTransactionForm.js
+++ bank-of-flatiron/src/components/AddTransactionForm.js
@@ -1,15 +1,53 @@
 import React, { Component } from "react";
 
+const baseURL = "http://localhost:6001/transactions/"
+
 class AddTransactionForm extends Component {
+  state = {
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  }
+
+  submitNewTransaction = (event) => {
+    event.preventDefault()
+
+    const newTransactionObj = {
+      ...this.state
+    }
+
+    const reqObj = {
+      headers: {"Content-Type": "application/json"},
+      method: "POST",
+      body: JSON.stringify(newTransactionObj)
+    }
+
+    fetch(baseURL, reqObj)
+    .then(resp => resp.json())
+    .then(submittedObj => {
+      this.props.addNewTransaction(submittedObj)
+      this.setState({
+        date: '',
+        description: '',
+        category: '',
+        amount: ''
+      })
+    })
+
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.submitNewTransaction} className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input value={this.state.date} onChange={(event) => this.setState({date: event.target.value})} type="date" name="date" />
+            <input value={this.state.description} onChange={(event) => this.setState({description: event.target.value})} type="text" name="description" placeholder="Description" />
+            <input value={this.state.category} onChange={(event) => this.setState({category: event.target.value})} type="text" name="category" placeholder="Category" />
             <input
+              value={this.state.amount} 
+              onChange={(event) => this.setState({amount: parseFloat(event.target.value)})}
               type="number"
               name="amount"
               placeholder="Amount"
