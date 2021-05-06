2acec59c38f34cf7f6867cd31ea3f4d3bceeb099
Sean Ngo <seanzngo@gmail.com>
2021-05-06 10:00:41 -0400

    core deliverables met i hope

--- bank-of-flatiron/src/components/AccountContainer.js
+++ bank-of-flatiron/src/components/AccountContainer.js
@@ -4,12 +4,30 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    searchText: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(r => r.json())
+    .then(transactions => this.setState({ transactions }))
+  }
+
+  addTransaction = transaction => this.setState({transactions: [transaction, ...this.state.transactions]})
+
+  changeSearchText = (text) => this.setState({searchText: text})
+
+  filterTransactions = () => this.state.transactions.filter(tran => tran.description.includes(this.state.searchText))
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchText={this.state.searchText} changeSearchText={this.changeSearchText} />
+        <AddTransactionForm addTransaction={this.addTransaction}/>
+        <TransactionsList transactions={this.filterTransactions()} />
       </div>
     );
   }
--- bank-of-flatiron/src/components/AddTransactionForm.js
+++ bank-of-flatiron/src/components/AddTransactionForm.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state={
+    date: "",
+    description: "",
+    category: "",
+    amount: 0
+  }
+
+  sendTransactionsToServer = (e) => {
+    e.preventDefault()
+
+    const reqObj = {
+            headers: {"Content-Type": "application/json"},
+            method: "POST",
+            body: JSON.stringify({...this.state})
+    }
+
+    fetch('http://localhost:6001/transactions', reqObj)
+    .then(res => res.json())
+    .then((transactionObj) => this.props.addTransaction(transactionObj))
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.sendTransactionsToServer} className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input onChange={(e) => this.setState({date: e.target.value})} type="date" name="date" />
+            <input onChange={(e) => this.setState({description: e.target.value})} type="text" name="description" placeholder="Description" />
+            <input onChange={(e) => this.setState({category: e.target.value})} type="text" name="category" placeholder="Category" />
             <input
+            onChange={(e) => this.setState({amount: e.target.value})}
               type="number"
               name="amount"
               placeholder="Amount"
