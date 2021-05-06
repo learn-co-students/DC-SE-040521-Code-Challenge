bd81bacb10b062640dc4000c60f51cd3e52b8fd4
menhaj <menhaj007@yahoo.com>
2021-05-06 10:59:49 -0400

    3 requirement check

--- bank-of-flatiron/src/components/AccountContainer.js
+++ bank-of-flatiron/src/components/AccountContainer.js
@@ -7,9 +7,11 @@ class AccountContainer extends Component {
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search changeSearchText={this.props.changeSearchText}  />
+        <AddTransactionForm addTransaction={this.props.addTransaction} />
+        <TransactionsList  accounts = {this.props.accounts} />
+        {/* {this.props.accounts.map(account => <TransactionsList key={account.key} accountData={account} />)} */}
+        
       </div>
     );
   }
